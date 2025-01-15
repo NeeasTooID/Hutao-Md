5f102b10bbab7cbbf03514d2c59a0b61c67598b9
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-15 07:34:36 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -7,11 +7,8 @@ global.tempDir = path.resolve(new URL('.', import.meta.url).pathname, tempName)
 
 global.adUrl = "https://raw.githubusercontent.com/kajedevid/dbku/refs/heads/main/adsku.json"
 
-global.pluginsUrl = "https://raw.githubusercontent.com/kajedevid/dbku/refs/heads/main/pluginsKu.json"
-
 const owner = [
-    ['6283897390164', 'NeofetchNpc'],
-    ['6289653162830']
+    ['6283897390164', 'NeofetchNpc']
 ]
 
 const defaultPrefix = ['/', '!', '#', '.', '-', '🗿']
